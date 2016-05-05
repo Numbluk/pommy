03770c1e757734ab99a191740faed6bee10a3bc4
Durendal <lukas.nimmo@gmail.com>
2016-05-05 01:28:08 -0500

    Added basic user profile information

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def labor_time_prettyfied
+    if @user.total_labor_time == 0
+      'Not Yet Begun'
+    else
+      (@user.total_labor_time.to_f / 60).to_s + ' Hrs.'
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,24 @@ class User < ActiveRecord::Base
                        format: { with: /\A[0-9a-zA-Z]+\z/, message: "can only contain letters and numbers." }
 
   has_secure_password
+
+  def total_labor_time
+    get_stages_by('labor').count * 30
+  end
+
+  def total_rest_time
+     get_stages_by('short_rest').count * 5 + total_clear() * 15
+  end
+
+  def total_stages_complete
+    self.stages.count
+  end
+
+  def total_clear
+    get_stages_by('long_rest').count
+  end
+
+  def get_stages_by(stage_type)
+    self.stages.where(stage_type: stage_type)
+  end
 end
